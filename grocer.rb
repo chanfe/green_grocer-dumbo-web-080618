5564d84167e52912e4e6fd5f1f74276dcf289b1f
Felix Chan <felixchan29@gmail.com>
2018-07-24 01:38:19 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,33 @@
+require 'pry'
 def consolidate_cart(cart)
   # code here
+  new_cart = {}
+  cart.each_with_index do |e, i|
+    e.each do |k, v|
+      v[:count] = cart.count(e)
+    end
+    new_cart.merge!(cart[i])
+  end
+
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  new_cart = {}
+  cart.each do |key, data|
+    for i in 0..coupons.length-1
+      if coupons[i][:item] == key && data[:count] >= coupons[i][:num]
+        data[:count] %= coupons[i][:num]
+        binding.pry
+        new_cart["#{key} W/COUPON"] = {:price => coupons[i][:cost], :clearance => cart[key][:clearance], :count => data[:count]/coupons[i][:num]}
+        if data[:count] == 0
+          cart.delete(key)
+        end
+      end 
+    end
+    #binding.pry
+  end
 end
 
 def apply_clearance(cart)
